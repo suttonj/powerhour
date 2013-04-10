132d520c4542f5259205c0dab1f948a4b391645f
Jeremy Sutton <jssutton11@gmail.com>
2013-04-09 20:03:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dionysus::Application.config.secret_token = '54b9f14214ce9ac138e2f69f11cf0d33444c234992bfbc4b11bd824153f0a3530d1eabceb6bc60bc5708bae5da94788e20e9d9e01193db093b03d9a7bafbda84'
