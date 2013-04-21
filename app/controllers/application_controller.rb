33804455ee988ecedcb02040d723f22ae4121529
Jeremy Sutton <jssutton11@gmail.com>
2013-04-21 18:30:29 -0400

    base ui layout in place, playlist title auto-update, working links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   	client = YouTubeIt::Client.new(:dev_key => ENV['API_KEY'])
   	playlist_videos = client.playlist(playlist_id)
   	playlist_videos_ids = playlist_videos.videos.map &:unique_id
+  	
+  	@playlistTitle = playlist_videos.title
   	@playlist = playlist_videos_ids
   	@playlist = @playlist.shuffle
   end
